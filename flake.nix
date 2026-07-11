099dafa4f7827da8b22ca5c4a29978c6e2432737
Tomas Carnecky <tomc@caurea.org>
2026-07-11 13:11:51 +0200

    Stop using flake-utils

--- flake.nix
+++ flake.nix
@@ -1,39 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
 
     nix-develop.url = "github:nicknovitski/nix-develop";
     nix-develop.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, nix-develop, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in {
-          packages.nix-develop = nix-develop.packages.${system}.default;
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      nix-develop,
+      ...
+    }:
+    let
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        nix-develop = nix-develop.packages.${pkgs.system}.default;
+      });
 
-          devShells.default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nodejs
-              pkgs.pnpm
-              pkgs.biome
-            ];
-          };
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.biome
+          ];
+        };
 
-          devShells.workflow = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nodejs
-              pkgs.pnpm
-              pkgs.biome
-              pkgs.jq
-            ];
-          };
-        }
-      );
+        workflow = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.biome
+            pkgs.jq
+          ];
+        };
+      });
+    };
 }
